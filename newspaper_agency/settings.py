b4a0600757d9aecb6c6b002d33ddf3863f4cccbd
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-18 15:56:43 +0300

    Added templates & static files

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "newspaper.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -150,3 +151,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ASSETS_ROOT = "/static/assets"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
